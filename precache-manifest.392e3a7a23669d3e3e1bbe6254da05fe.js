1b0c4ed145c4cbb758be10bc87715fe87d65fa62
izofiya <izofiya@mail.ru>
2019-05-18 03:27:25 +0300

    Updates

---
+++ precache-manifest.392e3a7a23669d3e3e1bbe6254da05fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9654368853b4e4eee21244734f68f857",
+    "url": "/Plescatsevich-Zoya-task-about-the-weather/index.html"
+  },
+  {
+    "revision": "aed3d42cf5e30ca60566",
+    "url": "/Plescatsevich-Zoya-task-about-the-weather/static/css/main.2d21bf90.chunk.css"
+  },
+  {
+    "revision": "a8528fc3385cb9d3f9d6",
+    "url": "/Plescatsevich-Zoya-task-about-the-weather/static/js/2.77f351f1.chunk.js"
+  },
+  {
+    "revision": "aed3d42cf5e30ca60566",
+    "url": "/Plescatsevich-Zoya-task-about-the-weather/static/js/main.1aa19293.chunk.js"
+  },
+  {
+    "revision": "e386ba4186ed65871381",
+    "url": "/Plescatsevich-Zoya-task-about-the-weather/static/js/runtime~main.3e3cfd1a.js"
+  }
+]);
